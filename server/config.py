c70972d15d982ed836e8337033e991b05cbc3a90
Christian <hernandezchristian94@gmail.com>
2023-06-05 23:07:27 -0700

    update

--- server/config.py
+++ server/config.py
@@ -12,23 +12,24 @@ from sqlalchemy import MetaData
 # Local imports
 
 # Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-# postgres://demo_game_v2_user:3c5birh6rxpSB2eSNqABmZTnwsF64pj4@dpg-chraa3grddlba9u8m8lg-a.oregon-postgres.render.com/demo_game_v2
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'yjekwsjrfy826592grhkljsdghfkuseygioebfoliwecvrg33p948576cnloutqovn'
-app.json.compact = False
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    # postgres://demo_game_v2_user:3c5birh6rxpSB2eSNqABmZTnwsF64pj4@dpg-chraa3grddlba9u8m8lg-a.oregon-postgres.render.com/demo_game_v2
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'yjekwsjrfy826592grhkljsdghfkuseygioebfoliwecvrg33p948576cnloutqovn'
+    app.json.compact = False
 
-# Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
-db.init_app(app)
+    # Define metadata, instantiate db
+    metadata = MetaData(naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    })
+    db = SQLAlchemy(metadata=metadata)
+    migrate = Migrate(app, db)
+    db.init_app(app)
 
-# Instantiate REST API
-api = Api(app)
+    # Instantiate REST API
+    api = Api(app)
 
-# Instantiate CORS
-CORS(app)
+    # Instantiate CORS
+    CORS(app)
